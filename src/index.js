58b6d06258de5c4f8fe5d45cd6c08187465c552d
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2022-11-18 05:18:01 -0500

    Static folder added

--- src/index.js
+++ src/index.js
@@ -2,22 +2,23 @@ import parser from 'body-parser'
 import cors from 'cors'
 import express from 'express'
 
-import { port } from './shared/configuration.js'
+import { directories, port } from './shared/configuration.js'
 
-const app = express()
+const server = express()
 
-app.use(cors({}))
-app.use(parser.json())
-app.use(parser.urlencoded({ extended: true }))
+server.use(express.static(directories.public))
 
-app.get('/', async function (request, response) {
-  response.json({ message: 'Welcome to the API!' })
+server.use(cors({}))
+server.use(parser.json())
+server.use(parser.urlencoded({ extended: true }))
+
+server.get('/user', async function (request, response) {
+  response.json({
+    name: 'John Doe',
+    email: 'john@example.com',
+  })
 })
 
-app.get('/user', async function (request, response) {
-  response.json({ message: 'Hi, I am a user!' })
-})
-
-app.listen(port, function () {
+server.listen(port, function () {
   console.log(`Server is running on port ${port}`)
 })
--- src/shared/configuration.js
+++ src/shared/configuration.js
@@ -1,9 +1,18 @@
 import { config } from 'dotenv'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
 config()
 
+const base = dirname(dirname(dirname(fileURLToPath(import.meta.url))))
+
+export const directories = {
+  base: base,
+  public: join(base, 'public'),
+}
+
 export const server = {
-  port: process.env.PORT,
+  port: process.env.SERVER_PORT,
 }
 
 export const port = server.port || 3000
