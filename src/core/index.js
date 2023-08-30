ade721662f894c737c826edd716347bc22dbdcc5
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2023-08-30 15:07:42 -0500

    Setup knex to get the user from database

---
+++ src/core/index.js
@@ -0,0 +1,6 @@
+import { Record } from './common/Record.js'
+import { User } from './users/User.js'
+import { UserRepository } from './users/UserRepository.js'
+import { UserService } from './users/UserService.js'
+
+export { Record, User, UserRepository, UserService }
